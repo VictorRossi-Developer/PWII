436acd525e38da283f20cb6052cd569c7544d142
Victor Yudi Rossi <yudi.marimoto@gmail.com>
2026-03-05 17:58:06 -0300

    Add files via upload

---
+++ Atividade3/atividade.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula - Exemplo do uso de variáveis</title>
+</head>
+<body>
+
+    <?php
+    $nome = "Victor";
+    $sobrenome = " Rossi";
+    echo $nome . $sobrenome;
+
+    $idade = "
+    16 anos";
+    echo $idade;
+
+    $data = "
+    16/06/2009
+    ";
+    echo $data;
+
+    ?>
+    
+</body>
+</html>
